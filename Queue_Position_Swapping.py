a9070c96ae6623eb3c256ccbab3a2a66b0cbdbe5
Veera Raghavendra Chikka, PhD <ch.raghavendra66@gmail.com>
2022-06-20 13:02:07 +0530

    Create Queue_Position_Swapping.py

---
+++ Queue_Position_Swapping.py
@@ -0,0 +1,10 @@
+'''
+Queue Position from HackerRank
+
+There are people waiting in line for a rollercoaster ride, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by from at the front of the line to at the back.Any person in the queue can ask the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can swap with at most two others.
+
+
+
+
+
+'''
