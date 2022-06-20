89d0b5cdcc950ac80b4ab78517904cc2233e2fe2
Veera Raghavendra Chikka, PhD <ch.raghavendra66@gmail.com>
2022-06-20 12:39:58 +0530

    Create Special_Substrings.py

---
+++ Special_Substrings.py
@@ -0,0 +1,20 @@
+'''
+Special Substrings from Hacker Rank
+
+A string is said to be a special string if either of two conditions is met:
+- All of the characters are the same, e.g. aaa.All characters except the middle one are the same, e.g. aadaa.
+- A special substring is any substring of a string which meets one of those criteria. 
+Given a string, determine how many special substrings can be formed from it.
+
+Input:
+5
+asasd
+
+Output:
+7
+
+{a, s, a, s, d, asa, sas}
+
+
+
+'''
